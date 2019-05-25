ea861c1f3edaad571dea2d4f52648bca9a4d3f08
Quentin <qkabasele@gmail.com>
2019-05-25 17:59:16 +0200

    Website design updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Dashboard from "./component/dashboard/Dashboard";
 import Admin from "./component/admin/Admin";
 import Client from "./component/client/Client";
 import ClientProfile from "./component/clientProfile/ClientProfile";
+import Footer from "./component/layout/Footer";
+import Signup from "./component/signup/Signup";
+import Signin from "./component/signin/Signin";
 
 class App extends Component {
   render() {
@@ -14,13 +17,16 @@ class App extends Component {
       <Router>
         <div className="body">
           <header>
-            {/* <Navbar /> */}
+            <Route path="/" component={Navbar} />
           </header>
           <Route exact path="/" component={Landing} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/signin" component={Signin} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/client" component={Client} />
           <Route exact path="/admin" component={Admin} />
           <Route exact path="/clientProfile" component={ClientProfile} />
+          <Footer/>
         </div>
       </Router>
     );
---
+++ src/component/signup/Signup.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstname:"",
+            lastname:"",
+            email:"",
+            client_name:"",
+            password:"",
+            confirmPassword:""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]:e.target.value});
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <main>
+                <div className="home">
+                    <h1 className="title">Inscription</h1>
+                    <p className="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem dolore iusto. Rerum error dolores consequuntur eius? Blanditiis, voluptatem, minima non quasi, accusamus sequi harum fuga dicta optio numquam dolor.
+                    </p>
+                </div>
+                <form onSubmit={this.handleSubmit} className="form">
+                    <input onChange={this.handleChange} type="text" name="firstname" id="firstname" placeholder="Entrer votre prénom"/>
+                    <input onChange={this.handleChange} type="text" name="lastname" id="lastname" placeholder="Entrer votre nom de famille"/>
+                    <input onChange={this.handleChange} type="email" name="email" id="email" placeholder="Entrer votre email"/>
+                    <input onChange={this.handleChange} type="text" name="client_name" id="client_name" placeholder="Entrer le nom de votre entreprise"/>
+                    <input onChange={this.handleChange} type="password" name="password" id="password" placeholder="Entrer votre mot de passe"/>
+                    <input onChange={this.handleChange} type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmer votre mot de passe"/>
+                    <input type="submit" className="btn btn-submit" value="Inscription"/>
+                </form>
+            </main>
+        )
+    }
+}
